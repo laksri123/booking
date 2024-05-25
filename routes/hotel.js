75950c94d9dec8b1cfb1a53d564852d3cf3ea691
someone <someone@someplace.com>
2024-05-25 22:28:04 +0530

    first commit

---
+++ routes/hotel.js
@@ -0,0 +1,29 @@
+import express from "express";
+  // import Hotel from "../models/Hotel.js";
+import {
+  countByCity,
+    createHotel,
+    deleteHotel,
+    countByType,
+    getHotelRooms,
+    getHotel,
+ getHotels,
+    updateHotel,
+  } from "../controllers/hotel.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
+
+
+
+const router = express.Router();
+
+router.post("/",verifyAdmin, createHotel);
+  
+
+ router.put("/:id",verifyAdmin, updateHotel );
+router.delete("/:id",verifyAdmin, deleteHotel);
+router.get("/find/:id", getHotel);
+router.get("/", getHotels);
+router.get("/countByCity", countByCity);
+router.get("/countByType", countByType);
+router.get("/room/:id", getHotelRooms);
+export default router;
